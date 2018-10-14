05c1caba4a09c0390f8c9e32dec4c28d305b1a9d
José Andrés Ceciliano Granados <cecilianogranados96@hotmail.com>
2018-10-14 10:21:04 -0600

    [Inicial]

---
+++ Examen/src/Caso2_Prototype/main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Caso2_Prototype;
+
+/**
+ *
+ * @author Jose
+ */
+public class main {
+
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/ii/examen_diseno/IIExamen_Diseno.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ii.examen_diseno;
+
+/**
+ *
+ * @author Jose
+ */
+public class IIExamen_Diseno {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
